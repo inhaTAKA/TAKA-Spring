90743b79209c7b49da81989ea33a67a11892b016
WooyoungKwon <rnjsdndud00@naver.com>
2024-10-19 16:25:33 +0900

    feat: login page

---
+++ src/test/java/com/example/napoli/CarpoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.napoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
