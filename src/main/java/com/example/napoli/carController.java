f31935f8e05b2b77274c24817a224122ff48a874
kwon <rnjsdndud00@naver.com>
2024-10-30 21:35:54 +0900

    fix: bootstrap 문법 삭제

---
+++ src/main/java/com/example/napoli/carController.java
@@ -0,0 +1,13 @@
+package com.example.napoli;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class carController {
+
+    @GetMapping("/carList")
+    public String carList(){
+        return "/car/carList";
+    }
+}
