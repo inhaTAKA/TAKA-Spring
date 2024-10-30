5470385751dbf2526b27804ce641203817325b3c
kwon <rnjsdndud00@naver.com>
2024-10-30 21:46:29 +0900

    안되면 자살함

--- src/main/java/com/example/napoli/carController.java
+++ src/main/java/com/example/napoli/carController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class carController {
-
     @GetMapping("/carList")
     public String carList(){
         return "/car/carList";
