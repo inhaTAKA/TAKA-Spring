32121335e6e381a99234c90c4f8c5d091b90dc20
tjdaniel1 <robot2daniel@gmail.com>
2024-11-07 01:36:20 +0900

    Feat: 자동차 생성 기능 추가

--- src/main/java/com/example/napoli/domain/repository/CarRepository.java
+++ src/main/java/com/example/napoli/domain/repository/CarRepository.java
@@ -4,5 +4,5 @@ import com.example.napoli.domain.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CarRepository extends JpaRepository<Car, String> {
-    Car findByCarId(Long carId);
+    Car findByCarId(String carId);
 }
